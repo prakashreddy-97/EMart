71c4752e98202626742ff5979d008033c4d73ff0
prakash <mamidisaiprakash@gmail.com>
2019-06-25 14:14:37 -0500

    added checkbox for password visibility in register page

--- Codes/register.js
+++ Codes/register.js
@@ -7,21 +7,21 @@ function Validate() {
     var confirmPassword = document.getElementById("cnfpd").value;     
     if(fName =="" || lName == "" || eMail == "" || password == ""|| confirmPassword ==""){
         alert("Please enter all the fields");
-<<<<<<< HEAD
+        return;
        }else{
            if(password != confirmPassword){
-               alert ("Incorrect Password");
+               alert ("Password and confirm password doesn't match");
            }
-=======
-        return;    
->>>>>>> 819a70b511d19a0955ea61c9d7ebc9a8da16f61c
+            
        }
-    if(password !== confirmPassword){
-        alert("Password and confirm password do not match, please check once again!");
-        return;
+    
     }
-    if(password.length<8){
-        alert("Weak Password!!");
-        return;
+
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
     }
-}
+  }
