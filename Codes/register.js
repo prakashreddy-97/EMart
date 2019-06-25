3df3b786411be8094a815af557cb6b89b932f763
prakash <mamidisaiprakash@gmail.com>
2019-06-25 14:33:46 -0500

    added password validations in register page

--- Codes/register.js
+++ Codes/register.js
@@ -1,27 +1,34 @@
 
 function Validate() {
-    var fName = document.getElementById("fname").value;
-    var lName = document.getElementById("lname").value;
-    var eMail = document.getElementById("email").value;
-    var password = document.getElementById("pd").value;
-    var confirmPassword = document.getElementById("cnfpd").value;     
-    if(fName =="" || lName == "" || eMail == "" || password == ""|| confirmPassword ==""){
-        alert("Please enter all the fields");
-        return;
-       }else{
-           if(password != confirmPassword){
-               alert ("Password and confirm password doesn't match");
-           }
-            
-       }
+  var fName = document.getElementById("fname").value;
+  var lName = document.getElementById("lname").value;
+  var eMail = document.getElementById("email").value;
+  var password = document.getElementById("pd").value;
+  var confirmPassword = document.getElementById("cnfpd").value;
+
+  if (fName == "" || lName == "" || eMail == "" || password == "" || confirmPassword == "") {
+    alert("Please enter all the fields");
+    return;
+  } else if (password != confirmPassword) {
+    alert("Password and confirm password doesn't match");
+  } 
+  
+
+}
     
-    }
+    
 
 function myFunction() {
-    var x = document.getElementById("myInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("cnfpd");
+  var y = document.getElementById("pd");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+  if (y.type === "password") {
+    y.type = "text";
+  } else {
+    y.type = "password";
+  }
+}
