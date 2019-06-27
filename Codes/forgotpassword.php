a1797c48d6fe0e384638444919ddda2d1ea982b7
Dasarivenkat03 <dasari.sai24@gmail.com>
2019-06-26 21:36:38 -0500

    Modified forgot password PHP code and made necessary changes

--- Codes/forgotpassword.php
+++ Codes/forgotpassword.php
@@ -1,16 +1,31 @@
 //PHP code for forgot password
 <?php
-if(isset($_POST) & !empty($_POST)){
-	$username = mysqli_real_escape_string($connection, $_POST['username']);
-	$sql = "SELECT * FROM `login` WHERE username = '$username'";
-	$res = mysqli_query($connection, $sql);
-	$count = mysqli_num_rows($res);
-	if($count == 1){
-		echo "Send email to user with password";
-	}else{
-		echo "User name does not exist in database";
-	}
-}
+	if(!empty($_POST["forgot-password"])){
+		$conn = mysqli_connect("localhost", "root", "", "blog_samples");
+		
+		$condition = "";
+		if(!empty($_POST["user-login-name"])) 
+			$condition = " member_name = '" . $_POST["user-login-name"] . "'";
+		if(!empty($_POST["user-email"])) {
+			if(!empty($condition)) {
+				$condition = " and ";
+			}
+			$condition = " member_email = '" . $_POST["user-email"] . "'";
+		}
+		
+		if(!empty($condition)) {
+			$condition = " where " . $condition;
+		}
 
+		$sql = "Select * from members " . $condition;
+		$result = mysqli_query($conn,$sql);
+		$user = mysqli_fetch_array($result);
+		
+		if(!empty($user)) {
+			require_once("forgot-password-recovery-mail.php");
+		} else {
+			$error_message = 'No User Found';
+		}
+	}
 ?>
 
