1a58a891eae9ad72d27aa851a53b3210adb4bee5
prakash <mamidisaiprakash@gmail.com>
2019-06-27 13:59:42 -0500

    Changes to be made

--- Codes/forgotpassword.php
+++ Codes/forgotpassword.php
@@ -1,16 +1,9 @@
-//PHP code for forgot password
-<?php
-if(isset($_POST) & !empty($_POST)){
-	$username = mysqli_real_escape_string($connection, $_POST['username']);
-	$sql = "SELECT * FROM `login` WHERE username = '$username'";
-	$res = mysqli_query($connection, $sql);
-	$count = mysqli_num_rows($res);
-	if($count == 1){
-		echo "Send email to user with password";
-	}else{
-		echo "User name does not exist in database";
-	}
-}
-
-?>
-
+<h2 class="form-signin-heading">Forgot Password</h2>
+        <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
+	  <input type="text" name="username" class="form-control" placeholder="Username" required>
+	</div>
+	<br />
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Forgot Password</button>
+        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
+      </form>
