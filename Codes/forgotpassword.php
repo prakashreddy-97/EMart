efa510d6dc9e3a10dc854e11bbe62f10d6380833
prakash <mamidisaiprakash@gmail.com>
2019-06-28 04:35:43 -0500

    made few changes

--- Codes/forgotpassword.php
+++ Codes/forgotpassword.php
@@ -12,33 +12,32 @@ $emailTo = $_POST["emailid"];
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
+
   
-    $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'emart.ecommercesite@gmail.com';                     // SMTP username
-    $mail->Password   = 'St@rk_Tech7';                               // SMTP password
-    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.gmail.com';  
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'emart.ecommercesite@gmail.com';              
+    $mail->Password   = 'St@rk_Tech7';                              
+    $mail->SMTPSecure = 'tls';                                 
+    $mail->Port       = 587;                                    
 
     //Recipients
     $mail->setFrom('emart.ecommercesite@gmail.com', 'Emart Support');
-    $mail->addAddress($emailTo);     // Add a recipient
+    $mail->addAddress($emailTo);     
 
     $mail->addReplyTo('no-reply@gmail.com', 'No reply');
     
 
 
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isHTML(true);                              
     $mail->Subject = 'Reset Password';
-    $mail->Body    = ' ';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+    $mail->Body    = 'Hey';
     $mail->send();
-    echo 'Message has been sent';
+    echo 'A link to reset your password has been sent to your email';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+exit();
 }
