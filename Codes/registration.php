25abfb13ce4a82d053cbe3196fa01023b67422da
Havya Ravipati <S533630@nwmissouri.edu>
2019-07-01 21:15:18 -0500

    updated registration.php

--- Codes/registration.php
+++ Codes/registration.php
@@ -1,6 +1,13 @@
+
 <html>
 <head>
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+$mail = new PHPMailer(true);
 // Passing the input values
 $error = Null;
 if(isset($_POST['register'])){
@@ -27,36 +34,26 @@ if(isset($_POST['register'])){
         $emailid = $connect->real_escape_string($emailid);
         $confirmpassword = $connect->real_escape_string($confirmpassword);
         //encryption of the verification key
-        $vkey = md5(time().$firstname.$lastname);
+        $vkey = rand(100000,999999);
         //encryption of the password
-        //$password = md5($password);
-        $password = sha1($password);
-        
+        $password = md5($password);
         //if the email exist in the database
         $query = "select emailid from customer where emailid = '".$emailid."'";
         $sol = mysqli_query($connect, $query);
         $numberofrows = mysqli_num_rows($sol);
         if ($numberofrows !== 0) {
-            header('Refresh: 2; url=register.html');
             echo "EMail already exists. Try again";
+            header('Refresh: 2; url=register.html');
+    
         } else {
             //new registration
             $insert = "insert into customer(firstname, lastname, emailid, password, vkey) values ('".$firstname."','".$lastname."','".$emailid."','".$password."','".$vkey."')";
-            echo($insert);
             mysqli_query($connect, $insert);
-            echo("Before if loop");
             // if insert successfull send the email to the dedicated email id.
-            if($insert){
-                echo("Inside if loop");
-                $to = $emailid;
-                $subject = "Email Confirmation";
-                $message = "<a href = 'http://localhost/emart/codes/verify.php?vkey=$vkey'>Register Account</a>";
-                $headers = "From: emart.ecommercesite@gmail.com \r\n";
-                //$headers = "MIME-Version: 1.0" . "\r\n";
-                //$headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
-                mail($to, $subject, $message, $headers);
-                echo "Email has been sent";
-                header('Refresh: 2; url:validateEmail.html');            
+            if($insert){   
+                
+                        
+                             
             }
             else{
                 echo $connect->error;
