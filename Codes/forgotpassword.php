8cd7ff2b7ca735670c17746a5397d7f9f26bad6d
aakash <s533714@nwmissouri.edu>
2019-07-01 20:33:39 -0500

    updated forgotpassword.php checking whether the email is existing in the databse or not

--- Codes/forgotpassword.php
+++ Codes/forgotpassword.php
@@ -1,31 +1,74 @@
-//PHP code for forgot password
 <?php
-	if(!empty($_POST["forgot-password"])){
-		$conn = mysqli_connect("localhost", "root", "", "blog_samples");
-		
-		$condition = "";
-		if(!empty($_POST["user-login-name"])) 
-			$condition = " member_name = '" . $_POST["user-login-name"] . "'";
-		if(!empty($_POST["user-email"])) {
-			if(!empty($condition)) {
-				$condition = " and ";
-			}
-			$condition = " member_email = '" . $_POST["user-email"] . "'";
-		}
-		
-		if(!empty($condition)) {
-			$condition = " where " . $condition;
-		}
+// Import PHPMailer classes into the global namespace
+// These should be present at the top not inside the function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-		$sql = "Select * from members " . $condition;
-		$result = mysqli_query($conn,$sql);
-		$user = mysqli_fetch_array($result);
-		
-		if(!empty($user)) {
-			require_once("forgot-password-recovery-mail.php");
-		} else {
-			$error_message = 'No User Found';
-		}
+if(isset($_POST["emailid"]))
+{
+	$emailTo = $_POST["emailid"];
+	$mail = new PHPMailer(true);
+
+	try 
+	{
+
+
+	$con = mysqli_connect('localhost', 'root', '', 'emart');
+
+    if (!$con) {
+        echo "<p style='color: red;'>Error connecting to database: </p>" .mysqli_error($con);
+        exit();
 	}
-?>
+	
 
+
+	$query = "select * from customer where emailid = '".$emailTo."'";
+    $sol = mysqli_query($con, $query);
+    $numberofrows = mysqli_num_rows($sol);
+ 
+
+
+    if ($numberofrows < 1) {     
+		echo "<script>alert(' User With This Email Does Not Exit');</script>";
+       // header('Location: forget-password.html');         // redirects to home page of emart
+	}
+    else
+    {
+    $mail->isSMTP();                                            
+    $mail->Host       = 'smtp.gmail.com';  
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'emart.ecommercesite@gmail.com';              
+    $mail->Password   = 'St@rk_Tech7';                              
+    $mail->SMTPSecure = 'tls';                                 
+    $mail->Port       = 587;                                    
+
+    //Recipients
+    $mail->setFrom('emart.ecommercesite@gmail.com', 'Emart Support');
+    $mail->addAddress($emailTo);     
+
+    $mail->addReplyTo('no-reply@gmail.com', 'No reply');
+    
+
+
+    // Content
+    $mail->isHTML(true);                              
+    $mail->Subject = 'Reset Password';
+    $mail->Body    = '<a href = "http://localhost/EMart/Codes/passwordReset.html" >Click here</a>';
+    $mail->send();
+	echo "<script>alert('A link to reset your password has been sent to your email');</script>
+		   <h1>Please check your mail for reset link</h1>
+		   <a href = 'http://localhost/EMart/Codes/login.html' >Login</a>";
+
+	
+    }
+}
+catch (Exception $e)
+{
+    echo "<script>alert(Message cannot   be sent. Mailer Error: {$mail->ErrorInfo});</script>";
+}
+exit();
+}
+?>
