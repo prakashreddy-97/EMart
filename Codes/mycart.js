0389f649a0f856beaeb5cf0df62277d80cd12215
Boppidi Jyoshna <s533706@nwmissouri.edu>
2019-09-22 19:35:21 -0500

    updated mycart.js

--- Codes/mycart.js
+++ Codes/mycart.js
@@ -23,8 +23,51 @@ function decreaseValue() {
   document.getElementById('price').innerHTML = totalDecrease;
 }
 
-function deleteitem(){
+function increaseValue1() {
+  var value = parseInt(document.getElementById('number1').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number1').value = value;
+  qty = Number(document.getElementById('number1').value);
+  totalIncrease = qty * 30;
+  document.getElementById('price1').innerHTML = totalIncrease;
  
-    var elem = document.getElementById('delete');
-    elem.parentNode.removeChild(elem);
-   }
+}
+
+
+
+
+function decreaseValue1() {
+  var value = parseInt(document.getElementById('number1').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number1').value = value;
+  qty = Number(document.getElementById('number1').value);
+  totalDecrease = qty * 30;
+  document.getElementById('price1').innerHTML = totalDecrease;
+}
+
+function increaseValue2() {
+  var value = parseInt(document.getElementById('number2').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number2').value = value;
+  qty = Number(document.getElementById('number2').value);
+  totalIncrease = qty * 50;
+  document.getElementById('price2').innerHTML = totalIncrease;
+ 
+}
+
+
+function decreaseValue2() {
+  var value = parseInt(document.getElementById('number2').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number2').value = value;
+  qty = Number(document.getElementById('number2').value);
+  totalDecrease = qty * 50;
+  document.getElementById('price2').innerHTML = totalDecrease;
+}
+
