30a5b08a51e95c392153f328dbbe49dcb4723b2b
ramesh nutulapathi <s534190@nwmissouri.edu>
2019-09-28 18:54:56 -0500

    laptops

---
+++ Codes/laptops/templates/top.php
@@ -0,0 +1,7 @@
+<nav class='navbar  navbar-light ' style='background-color: #e3f2fd;'>
+  <a class='navbar-brand' href='/emart/Codes/adminhome.html'>Home</a>
+
+  <a class='nav-link' href='index.php'>Products</a> <a class='nav-link' href='add-record.php'>Add Record</a> <a class='nav-link' href='/emart/Codes/emart.html'>Sign Out</a>
+   
+  
+</nav>
