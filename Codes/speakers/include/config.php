7d07291f6225af0533adee951e215aa73a3eb4ce
Havya Ravipati <S533630@nwmissouri.edu>
2019-09-28 15:57:11 -0500

    added config.php

---
+++ Codes/speakers/include/config.php
@@ -0,0 +1,11 @@
+<?Php
+$host_name = "localhost";
+$database = "emart"; // Change your database name
+$username = "root";          // Your database user id 
+$password = "";    // Your password
+
+//error_reporting(0);// With this no error reporting will be there
+//////// Do not Edit below /////////
+
+
+?>
