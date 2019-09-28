f425eb578c9603d5febb4ea37d4f5707e1a467a9
Havya Ravipati <S533630@nwmissouri.edu>
2019-09-28 15:58:56 -0500

    made changes to config-pdo.php

--- Codes/speakers/include/config-pdo.php
+++ Codes/speakers/include/config-pdo.php
@@ -5,5 +5,10 @@ $username = "root";          // Your database user id
 $password = "";    // Your password
 
 //////// Do not Edit below /////////
-
+try {
+    $dbo = new PDO('mysql:host='.$host_name.';dbname='.$database, $username, $password);
+    } catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+    }
 ?>
