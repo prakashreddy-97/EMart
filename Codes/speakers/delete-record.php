9f244154c153a5f35029ff880ad4cc7083889465
Havya Ravipati <S533630@nwmissouri.edu>
2019-09-28 16:09:59 -0500

    updated delete-record.php

--- Codes/speakers/delete-record.php
+++ Codes/speakers/delete-record.php
@@ -17,5 +17,26 @@ if($stmt = $connection->prepare("SELECT img FROM speakers  WHERE p_id=?")){
   $elements['msg'].=$connection->error;
 }
 ////Delete record from table ///
+$query="DELETE FROM speakers WHERE p_id=?";
+$stmt = $connection->prepare($query);
+if ($stmt) {
+$stmt->bind_param('i', $p_id);
+$stmt->execute();
+$elements['msg'].=" Record Deleted <br>";
+$elements['records_affected']=$stmt->affected_rows;
+}else{
+$elements['msg'].=$connection->error;
+}
+/////
 
+
+///
+if($elements['records_affected']==1){
+	if(@unlink("images/$file_name")) {$elements['msg'].=" File Deleted "; }
+else{$elements['msg'].=" File Not Deleted ";}
+}
+/// Post back data /////
+
+$elements['db_status']="True";
+echo json_encode($elements);
 ?>
