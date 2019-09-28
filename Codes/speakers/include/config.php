27a0327832294f041e931445f48792110b0e1dc2
Havya Ravipati <S533630@nwmissouri.edu>
2019-09-28 15:57:41 -0500

    made changes to config.php

--- Codes/speakers/include/config.php
+++ Codes/speakers/include/config.php
@@ -7,5 +7,12 @@ $password = "";    // Your password
 //error_reporting(0);// With this no error reporting will be there
 //////// Do not Edit below /////////
 
+$connection = mysqli_connect($host_name, $username, $password, $database);
 
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL.<br>";
+    echo "<br>Debugging errno: " . mysqli_connect_errno();
+    echo "<br>Debugging error: " . mysqli_connect_error();
+    exit;
+}
 ?>
