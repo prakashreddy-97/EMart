f6756e901f105d9daacf0b3f2d8b0128924fd984
PrakashReddy <mamidisaiprakash@gmail.com>
2019-09-28 15:34:19 -0500

    Made changes to admin login

--- Codes/validation.php
+++ Codes/validation.php
@@ -33,12 +33,20 @@ if ($status){
         exit();
     }
     $query = "select * from customer where emailid = '".$username."' && password = '".md5($password)."' && verified = 1" ;
+    $isAdminquery = "select AdminAccess from customer  where emailid = '".$username."' && password = '".md5($password)."' && verified = 1";
+
     $sol = mysqli_query($con, $query);
+    $isAdmin = mysqli_query($con,$isAdminquery);
     $numberofrows = mysqli_num_rows($sol);
     if ($numberofrows == 1) {     
         echo "Login Successful";    //if credentials match
-      
-        header('Location: emart.html');         // redirects to home page of emart
+        $_SESSION['loggedIn'] = true;
+        if($isAdmin == 0){
+        header('Location: emart.html');  
+        }       // redirects to home page of emart
+        else{
+            header('Location: adminhome.html');
+        }// redirect to admin home page of emart
     } else {       
         header('Refresh: 2; url=login.html');
        echo "Wrong Username or Password. Try Again.";
