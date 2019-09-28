cf12a0beb99a339cb8af071ddd05f99b4a6c93ed
Sai Sri Lakshmi Vancha <S533905@nwmissouri.edu>
2019-09-28 16:34:10 -0500

    updated mobile&Accessories

---
+++ Codes/mobiles&accessories/index.php
@@ -0,0 +1,72 @@
+<?Php
+echo "<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+
+    <title>EMart Administrator  </title>";
+require "templates/head.php";
+echo "<link rel='stylesheet' href='templates/custom.css' type='text/css'>
+<style>
+.my_div  {
+vertical-align: middle;
+}
+.my_table {width:700px;}
+</style>
+</head><body>";
+
+require "templates/top.php";
+
+
+require "include/config.php";
+////////////
+echo "<div class='alert alert-danger alert-dismissible fade show' id=msg_display> </div>";
+
+echo "<div class='row'>
+<div class='col-md-11 offset-md-1'>";
+
+if($stmt = $connection->query("SELECT * FROM mobilesandaccessories")){
+$no=$stmt->num_rows;
+  echo "No of records : <span id=no>$no</span><br>";
+
+
+while ($row = $stmt->fetch_assoc()) {
+echo "<div class='row align-middle' id=d_$row[p_id] >
+<div class='col-md-3'><img src=images/$row[img] class='square' alt='$row[p_name]'></div>
+<div class='col-md-2'>$row[p_name]</div>
+<div class='col-md-1'>$row[p_id]</div>
+<div class='col-md-1'>$row[price]</div>
+<div class='col-md-1'><span id=$row[p_id] class=del><img src=delete.jpg></span></div>
+</div>";
+}
+
+}else{
+echo $connection->error;
+}
+echo "</div></div>";
+
+require "templates/bottom.php";	
+?>
+
+<script>
+$(document).ready(function() {
+/////////// form submission//
+$('.del').click(function(){
+var id=$(this).attr('id');
+$.get('delete-record.php',{'p_id':id,'todo':'delete'},function(return_data){
+if(return_data.records_affected == 1){
+$("#d_"+id).hide();
+// Number of records to decrease by one
+var no=parseInt($('#no').html())-1; 
+$('#no').html(no);
+}	
+$("#msg_display").html(return_data.msg);     
+$("#msg_display").show();
+setTimeout(function() { $("#msg_display").fadeOut('slow'); }, 4000);
+},"json");
+})
+////
+})
+</script>
+</body>
+</html>
