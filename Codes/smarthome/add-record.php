3f4493cede40c159323a70a2f8c4c736714734e7
Dasarivenkat03 <dasari.sai24@gmail.com>
2019-09-28 18:45:38 -0500

    backend smart home

---
+++ Codes/smarthome/add-record.php
@@ -0,0 +1,86 @@
+<?Php
+echo "<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+
+    <title>EMart.com </title>";
+require "templates/head.php";
+echo "<link rel='stylesheet' href='templates/custom.css' type='text/css'>
+<style>
+.my_div  {
+vertical-align: middle;
+}
+.my_table {width:700px;}
+</style>
+</head><body>";
+
+require "templates/top.php";
+
+
+require "include/config.php";
+////////////
+echo "<div class='alert alert-danger alert-dismissible fade show' id=msg_display> </div>";
+
+echo "<br><div class='row'>
+<div class='col-md-11 offset-md-1'>";
+echo "
+<form id='data' method='post' enctype='multipart/form-data'>
+
+<div class='row'>
+  <div class='col-sm-2 offset-sm-3'>Name</div>
+  <div class='col-sm-2'><input type='text' class='form-control' id='userid' name=p_name placeholder='Product Name'></div>
+</div>
+<br>
+<div class='row'>
+  <div class='col-sm-2 offset-sm-3'>Price (Enter floating value xx.xx)</div>
+  <div class='col-sm-2'><input type='text' class='form-control' id='userid' name=price placeholder='Price'></div>
+</div>
+<br>
+<div class='row'>
+  <div class='col-sm-2 offset-sm-3'>Upload file(Image size should be less than 250kb)</div>
+  <div class='col-sm-4'><input type=file name='file_up'></div>
+</div>
+
+<br>
+<div class='row'>
+  <div class='col-sm-2 offset-sm-3'></div>
+  <div class='col-sm-4'><button>Submit</button></FORM></div>
+</div>
+
+";
+
+echo "</div></div>";
+
+require "templates/bottom.php";	
+?>
+
+<script>
+$(document).ready(function() {
+/////////// form submission//
+$("form#data").submit(function(e) {
+ e.preventDefault();    
+ var formData = new FormData(this);
+ $.ajax({
+  url: 'add-recordck.php',
+  type: 'POST',
+  data: formData,
+  dataType: 'json',
+  success: function (return_data) {
+   if(return_data.validation_status=='T'){
+	$('form#data').trigger("reset");
+   }
+  $("#msg_display").html(return_data.msg);     
+  $("#msg_display").show();
+  setTimeout(function() { $("#msg_display").fadeOut('slow'); }, 4000);
+     },
+   cache: false,
+   contentType: false,
+   processData: false
+  });
+});
+////
+})
+</script>
+</body>
+</html>
---
+++ Codes/smarthome/delete-record.php
@@ -0,0 +1,42 @@
+<?Php
+$p_id=$_GET['p_id'];
+$todo=$_GET['todo'];
+$elements=array("p_id"=>"$p_id","db_status"=>"","msg"=>"","records_affected"=>"");
+require "include/config.php"; // Database connection 
+
+/// Collect the file name of image /////
+if($stmt = $connection->prepare("SELECT img FROM smarthome  WHERE p_id=?")){
+  $stmt->bind_param('i',$p_id);
+  $stmt->execute();
+   
+   $result = $stmt->get_result();
+   //echo "No of records : ".$result->num_rows."<br>";
+   $row=$result->fetch_object();
+   $file_name=$row->img;
+}else{
+  $elements['msg'].=$connection->error;
+}
+////Delete record from table ///
+$query="DELETE FROM smarthome WHERE p_id=?";
+$stmt = $connection->prepare($query);
+if ($stmt) {
+$stmt->bind_param('i', $p_id);
+$stmt->execute();
+$elements['msg'].=" Record Deleted <br>";
+$elements['records_affected']=$stmt->affected_rows;
+}else{
+$elements['msg'].=$connection->error;
+}
+/////
+
+
+///
+if($elements['records_affected']==1){
+	if(@unlink("images/$file_name")) {$elements['msg'].=" File Deleted "; }
+else{$elements['msg'].=" File Not Deleted ";}
+}
+/// Post back data /////
+
+$elements['db_status']="True";
+echo json_encode($elements);
+?>
