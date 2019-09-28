0f1753f6ea75c89c31248e53cefacc0d7390b68b
Havya Ravipati <S533630@nwmissouri.edu>
2019-09-28 15:58:24 -0500

    added config-pdo.php

---
+++ Codes/speakers/include/config-pdo.php
@@ -0,0 +1,9 @@
+<?Php
+$host_name = "localhost";
+$database = "emart"; // Change your database name
+$username = "root";          // Your database user id 
+$password = "";    // Your password
+
+//////// Do not Edit below /////////
+
+?>
