e732aea0d61e626f555aedf9e4034a0593d2403b
ramesh nutulapathi <s534190@nwmissouri.edu>
2019-10-04 09:22:53 -0500

    abc

---
+++ Codes/tablets/add-recordck.php
@@ -0,0 +1,68 @@
+<?Php
+$p_name=$_POST['p_name'];
+$price=$_POST['price'];
+$description=$_POST['description'];
+$elements=array("msg"=>"","records_affected"=>"","validation_status"=>"T");
+
+//Check price format 
+if(!filter_var($price,FILTER_VALIDATE_FLOAT)){
+$elements['msg'].=" Enter Price details <br>";	
+$elements['validation_status']="F";
+}
+
+// Check Product Name
+if(!ctype_alpha(str_replace(' ', '', $p_name)) === true){
+$elements['msg'].=" Enter Product Name without Special Chars <br>";		
+$elements['validation_status']="F";	
+}	
+
+// Check file type
+if (!($_FILES[file_up][type] =="image/jpeg" OR $_FILES[file_up][type] =="image/gif"))
+{$elements['msg'].="Your uploaded file must be of JPG or GIF. ";
+$elements['msg'].="Other file types are not allowed<BR>";
+$elements['validation_status']="F";	
+}
+
+// Check file size 
+if ($_FILES[file_up][size]>250000){
+$elements['msg'].="Your uploaded file size is more than 250KB ";
+$elements['msg'].=" so please reduce the file size and then upload.<BR>";
+$elements['validation_status']="F";	
+}
+
+$file_name=$_FILES[file_up][name];// 
+
+
+if($elements['validation_status']=="T"){
+
+// the path with the file name where the file will be stored
+$add="C:/xampp/htdocs/EMart/Codes/tablets/images/$file_name"; 
+if(move_uploaded_file ($_FILES[file_up][tmp_name], $add)){
+$elements['msg'].=" File successfully uploaded.<BR>";
+// Insert record to table with file name///
+require "include/config.php"; // Database connection 
+$query="INSERT INTO tablets (p_name,price,img,description) values('$p_name','$price','$file_name','$description')";
+$stmt=$connection->prepare($query);
+if($stmt){ 
+$stmt->bind_param("ss", $p_name,$file_name);
+if($stmt->execute()){
+$elements['msg'].= "Records added : ".$connection->affected_rows;
+$elements['msg'].= "<br>Product ID : ".$connection->insert_id;
+}else{
+$elements['validation_status']="F";		
+$elements['msg'].="Database error : ". $connection->error;
+}
+}else{
+$elements['validation_status']="F";		
+$elements['msg'].="Database error : ". $connection->error;
+}
+// End of insert record with file name ///
+}else{ // file upload by move_uploaded_file
+$elements['msg'].="Failed to upload file Contact Site admin to fix the problem";
+$elements['validation_status']="F";	
+}// file upload by move_uploaded_file
+
+}// if validation_status is equal to T	 
+
+echo json_encode($elements);
+?>
---
+++ Codes/tablets/user_index.php
@@ -0,0 +1,54 @@
+<?Php
+echo "<!doctype html>
+<html lang='en'>
+  <head>
+    <!-- Required meta tags -->
+
+    <title>EMart Administrator  </title>";
+require "templates/head.php";
+echo "<link rel='stylesheet' href='templates/custom.css' type='text/css'>
+<style>
+.my_div  {
+vertical-align: middle;
+}
+.my_table {width:700px;}
+</style>
+</head><body>";
+
+require "templates/user_top.php";
+
+require "templates/sidenav2.html";
+require "include/config.php";
+////////////
+echo "<div class='alert alert-danger alert-dismissible fade show' id=msg_display> </div>";
+
+echo "<div class='row'>
+<div class='col-md-11 offset-md-1'>";
+
+
+
+if($stmt = $connection->query("SELECT * FROM tablets")){
+$no=$stmt->num_rows;
+  
+while ($row = $stmt->fetch_assoc()) {
+echo "<div class='row align-middle' id=d_$row[p_id] >
+<div class='col-md-3'><img src=images/$row[img] class='square' alt='$row[p_name]'></div>
+<div class='col-md-2'><strong>$row[p_name]</strong></div>
+
+<div class='col-md-2'>$row[price]$</div>
+<div class='col-md-1'>$row[description]</div>
+<a class='col-md-0' href='/emart/Codes/emart.html'>Add to Cart</a>
+</div>";
+}
+
+}else{
+echo $connection->error;
+}
+echo "</div></div>";
+
+/*require "templates/bottom.php";	*/
+?>
+
+
+</body>
+</html>
