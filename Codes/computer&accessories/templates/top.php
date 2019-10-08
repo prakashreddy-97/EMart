9540fa8f76851d261da13541a0e575b3a4bfcc8b
Havya Ravipati <S533630@nwmissouri.edu>
2019-10-07 20:28:39 -0500

    altered the UI

--- Codes/computer&accessories/templates/top.php
+++ Codes/computer&accessories/templates/top.php
@@ -1,7 +1,7 @@
-<nav class='navbar  navbar-light ' style='background-color: #e3f2fd;'>
-  <a class='navbar-brand' href='/emart/Codes/adminhome.html'>Home</a>
+<nav class='navbar  navbar-light ' style='background-color: black;'>
+  <a class='navbar-brand' href='/emart/Codes/adminhome.html' style='color: white'>Home</a>
 
-  <a class='nav-link' href='index.php'>Products</a> <a class='nav-link' href='add-record.php'>Add Record</a> <a class='nav-link' href='/emart/Codes/emart.html'>Sign Out</a>
+  <a class='nav-link' href='index.php' style='color: white'>Products</a> <a class='nav-link' href='add-record.php' style='color: white'>Add Record</a> <a class='nav-link' href='/emart/Codes/emart.html' style='color: white'>Sign Out</a>
    
   
 </nav>
