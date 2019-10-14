6e9b66388408e4be3d0e126308116608fb9bb1e7
Sai Sri Lakshmi Vancha <S533905@nwmissouri.edu>
2019-10-13 20:30:45 -0500

    Edit product commit

--- Codes/tablets/index.php
+++ Codes/tablets/index.php
@@ -27,9 +27,7 @@ echo "<div class='row'>
 
 if($stmt = $connection->query("SELECT * FROM tablets")){
 $no=$stmt->num_rows;
-  echo "No of records : <span id=no>$no</span><br>";
-
-
+ 
 while ($row = $stmt->fetch_assoc()) {
 echo "<div class='row align-middle' id=d_$row[p_id] >
 <div class='col-md-3'><img src=images/$row[img] class='square' alt='$row[p_name]'></div>
