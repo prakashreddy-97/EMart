6b5616b90bf4e49a7244fd45b8ab486ee939ca80
ramesh nutulapathi <s534190@nwmissouri.edu>
2019-10-13 22:54:41 -0500

    work34

--- Codes/mycart.php
+++ Codes/mycart.php
@@ -16,6 +16,13 @@ if (!empty($product_array)) {
 
 	}
 }
+if (isset($_POST['action']) && $_POST['action']=="change"){
+	foreach($_SESSION["shopping_cart"] as &$value){
+	  if($value['code'] === $_POST["code"]){
+		  $value['quantity'] = $_POST["quantity"];
+		  break; // Stop the loop after we've found the product
+	  }
+  }
 
 session_start();
 $status="";
