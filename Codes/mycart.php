ad2d23941f0ad6cca50d9e21c4c62faf1560ae4e
Boppidi Jyoshna <s533706@nwmissouri.edu>
2019-10-20 10:24:22 -0500

    added code to list cart items

--- Codes/mycart.php
+++ Codes/mycart.php
@@ -1,8 +1,64 @@
+
+
+<div id="shopping-cart">
+<div class="txt-heading">Shopping Cart</div>
+
+<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:left;">Code</th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["code"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				</tr>
+				<?php
+				$total_quantity += $item["quantity"];
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+} else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php
+}
+?>
+</div>
+
 <?php
 $product_array = $db_handle->runQuery("SELECT * FROM tblproduct ORDER BY id ASC");
 if (!empty($product_array)) { 
 	foreach($product_array as $key=>$value){
-
+//creating product gallery
 	<div class="product-item">
 		<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
 		<div class="product-image"><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
@@ -39,7 +95,7 @@ if(!empty($_SESSION["shopping_cart"])) {
       } 
 }
 }
-
+//adding item to cart
 case "add":
 	if(!empty($_POST["quantity"])) {
 		$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
@@ -63,6 +119,7 @@ case "add":
 		}
 	}
 	break;
+	//remove item from cart
 	case "remove":
 	if(!empty($_SESSION["cart_item"])) {
 		foreach($_SESSION["cart_item"] as $k => $v) {
