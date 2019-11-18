afa101a68679ba799326c2cc92b3bf67c0aa65c9
aakash <s533714@nwmissouri.edu>
2019-11-17 23:47:09 -0600

    updated db controller.php and index.php

--- Codes/simple-php-shopping-cart-test/dbcontroller.php
+++ Codes/simple-php-shopping-cart-test/dbcontroller.php
@@ -29,5 +29,40 @@ class DBController {
 		$rowcount = mysqli_num_rows($result);
 		return $rowcount;	
 	}
+	case "add":
+		if(!empty($_POST["quantity"])) {
+			$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
+			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
+			
+			if(!empty($_SESSION["cart_item"])) {
+				if(in_array($productByCode[0]["code"],array_keys($_SESSION["cart_item"]))) {
+					foreach($_SESSION["cart_item"] as $k => $v) {
+							if($productByCode[0]["code"] == $k) {
+								if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+									$_SESSION["cart_item"][$k]["quantity"] = 0;
+								}
+								$_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+							}
+					}
+				} else {
+					$_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+				}
+			} else {
+				$_SESSION["cart_item"] = $itemArray;
+			}
+		}
+		break;
+	
+	
+	<div id="shopping-cart">
+	<div class="txt-heading">Shopping Cart</div>
+	
+	<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+	<?php
+	if(isset($_SESSION["cart_item"])){
+		$total_quantity = 0;
+		$total_price = 0;
+	?>	
+	
 }
 ?>
