461f2a45141a6b55313364cff19b4a0f0598bb13
PrakashReddy <mamidisaiprakash@gmail.com>
2019-12-05 18:51:28 -0600

    Delete history

--- Codes/tablets/delete-record.php
+++ Codes/tablets/delete-record.php
@@ -16,8 +16,24 @@ if($stmt = $connection->prepare("SELECT img FROM c_table  WHERE unique_id=?")){
 }else{
   $elements['msg'].=$connection->error;
 }
-////Delete record from table ///
-// $query="DELETE FROM tablets WHERE unique_id=?";
+if($stmt = $connection->prepare("SELECT p_name FROM c_table  WHERE unique_id=?")){
+  $stmt->bind_param('s',$unique_id);
+  $stmt->execute();
+   
+   $result = $stmt->get_result();
+   //echo "No of records : ".$result->num_rows."<br>";
+   $row=$result->fetch_object();
+   $p_name=$row->p_name;
+}else{
+  $elements['msg'].=$connection->error;
+}
+
+$date = date("Y-m-d");
+$historyQuery = "INSERT INTO adminHistory(p_name,`action`,img,category,dateModified) values('$p_name','Deleted','$file_name','tablets','$date')";
+$stmt2 = $connection->prepare($historyQuery);
+$stmt2->execute();
+
+
 $query = "DELETE FROM c_table WHERE unique_id=? and category ='tablets'";
 $stmt = $connection->prepare($query);
 // $stmt2 = $connection->prepare($query2);
@@ -28,6 +44,8 @@ $stmt->execute();
 // $stmt2->execute();
 $elements['msg'].=" Record Deleted <br>";
 $elements['records_affected']=$stmt->affected_rows;
+
+header("Refresh:0");
 }else{
 $elements['msg'].=$connection->error;
 }
